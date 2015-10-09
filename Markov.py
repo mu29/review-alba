eb80a2d7e1bf392799c7f6b4f88caba65743aa01
InJung Chung <mu29gl@gmail.com>
2015-10-09 21:56:36 +0900

    make comment

--- Markov.py
+++ Markov.py
@@ -1,17 +1,20 @@
 #-*- coding: utf-8 -*-
 
+import random
 from Node import *
 
 class Markov:
     def __init__(self):
-        self.nodes = []
+        self.nodes = {}
+        self.start_nodes = []
 
     def make_database(self, reviews):
         for review in reviews:
-            words = review.split()
+            words = review.split(' ')
             is_new_node = True
-            for i in range(0, len(words) - 1):
+            for i in range(0, len(words)):
                 word = words[i]
+                print word
                 node = None
                 if word in self.nodes:
                     node = self.nodes[word]
@@ -19,15 +22,32 @@ class Markov:
                 else:
                     node = Node()
                     if i == len(words) - 1: node.end = True
-                    if words[i].find('.'): node.end = True
-                    if words[i].find('!'): node.end = True
-                    if words[i].find('?'): node.end = True
-                    if words[i].find('.'): node.end = True
-                    if words[i].find(u'ㅋ'): node.end = True
+                    if words[i].find('.') > 0: node.end = True
+                    if words[i].find('!') > 0: node.end = True
+                    if words[i].find('?') > 0: node.end = True
+                    if words[i].find('.') > 0: node.end = True
+                    if words[i].find(u'ㅋ') > 0: node.end = True
 
-                    node.text = words[i]
+                    node.text = word
                     if node.end: is_new_node = True
-                    else: node.next = words[i + 1]
+                    else: node.next.append(words[i + 1])
 
-                    self.nodes.append(node)
-                if is_new_node: node.start = True
+                    self.nodes[word] = node
+                if is_new_node:
+                    node.start = True
+                    self.start_nodes.append(node)
+
+    def make_comment(self):
+        index = random.randrange(0, len(self.start_nodes))
+        start = self.start_nodes[index]
+
+        return self.make_text(start.text, start)
+
+    def make_text(self, text, node):
+        if node.end == True:
+            return text
+
+        index = random.randrange(0, len(node.next))
+        next_word = node.next[index]
+        text += next_word + " "
+        return self.make_text(text, self.nodes[next_word])
