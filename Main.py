f82cc82e1fe861c3c6e1431923ad7fe3c72bdf59
InJung Chung <mu29gl@gmail.com>
2015-10-09 22:12:41 +0900

    review data backup

--- Main.py
+++ Main.py
@@ -4,6 +4,10 @@ from Markov import *
 from Crawler import *
 
 crawler = Crawler()
-k = crawler.get_pos_reviews(1)
 markov = Markov()
-markov.make_database(k)
+
+reviews = crawler.get_pos_reviews(100)
+markov.make_database(reviews)
+
+for i in range(0, 10):
+    print markov.make_comment()
